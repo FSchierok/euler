8b45f4d39f41d50fd2ca29ead4b900b2274126e4
Fabian Schierok <fabian.schierok@tu-dortmund.de>
2018-06-15 18:21:49 +0200

    first part of new method

--- p12.py
+++ p12.py
@@ -15,6 +15,18 @@ def numDiv(n):
     return count
 
 
+def numDiv2(n):
+    prims = [2, 3, 5, 7, 11, 13, 17, 19]
+    exp = list()
+    i = 1
+    for prim in prims:
+        if n % prim**i == 0:
+            i += 1
+        else:
+            exp.append(i - 1)
+    return("??")
+
+
 TRI = triangle()
 for tri in TRI:
     if numDiv(tri) == 500:
