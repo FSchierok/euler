8e53dd31a3727e417b27c6c68e288a8035227b39
Fabian Schierok <fabian.schierok@udo.edu>
2020-08-09 23:43:53 +0200

    Update P12

--- p12.py
+++ p12.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def triangle():
     num = 0
     i = 1
@@ -7,30 +10,27 @@ def triangle():
         yield num
 
 
-def numDiv(n):
-    count = 0
-    for i in range(1, (n // 2) + 1):
+def primeFak(n):
+    primes = dict()
+    for i in range(2, n+1):
         if n % i == 0:
-            count += 1
-    return count
+            primes[i] = primes.get(i, 0) + 1
+            rek = primeFak(int(n / i))
+            for prime, cnt in rek.items():
+                primes[prime] = primes.get(prime, 0) + cnt
+            break
+    return primes
 
 
-def numDiv2(n):
-    prims = [2, 3, 5, 7, 11, 13, 17, 19]
-    exp = list()
-    for prim in prims:
-        i = 0
-        if n % prim**i == 0:
-            i += 1
-        else:
-            exp.append(i - 1)
-    return("??")
-# Binom: n über i ist anzahl um i aus n zu ziehen
-# summe der Eponenten für alle Teiler aus einer Primzahl, + Binom von zahl der Primzahlen über 1-Zahl + Mischterme mit potenzen
-
+def multPlusOne(arr):
+    tmp = np.array(arr)
+    tmp += 1
+    return np.prod(tmp)
 
 TRI = triangle()
 for tri in TRI:
-    if numDiv(tri) == 500:
+    primeDict = primeFak(tri)
+    primes = list(primeDict.values())
+    if multPlusOne(primes) >= 500:
         print(tri)
         break
